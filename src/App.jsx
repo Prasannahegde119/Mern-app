a490a5c8e216ea3b35b18e0a494bb6961549255b
prasanna <hegdeprasanna118@gmail.com>
2024-04-05 10:59:58 +0530

    helmet added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
+// import React from 'react';
+// import Navbar from './Components/Navbar/Navbar';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Hero from './Components/Hero/Hero';
+// import Contact from './Components/Contact/Contact';
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path='/' element={<div><Navbar /><Hero /></div>} />
+//         <Route path='/Contact' element={<Contact/>} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+
 import React from 'react';
-import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Helmet from 'react-helmet';
+import Navbar from './Components/Navbar/Navbar';
 import Hero from './Components/Hero/Hero';
 import Contact from './Components/Contact/Contact';
 
@@ -8,7 +29,16 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<div><Navbar /><Hero /></div>} />
+        <Route path='/' element={
+          <div>
+            <Helmet>
+              <title>Home Page Title</title>
+              <meta name="description" content="This is a description of the home page." />
+            </Helmet>
+            <Navbar />
+            <Hero />
+          </div>
+        } />
         <Route path='/Contact' element={<Contact/>} />
       </Routes>
     </BrowserRouter>
--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,9 +1,27 @@
+// import React from 'react'
+// import './contact.css'
+
+// const Contact = () => {
+//   return (
+//     <div className='contact'>
+//       <h1>prasanna</h1>
+//     </div>
+//   )
+// }
+
+// export default Contact
+
 import React from 'react'
+import { Helmet } from 'react-helmet';
 import './contact.css'
 
 const Contact = () => {
   return (
     <div className='contact'>
+      <Helmet>
+        <title>Contact Page Title</title>
+        <meta name="description" content="This is a description of the contact page." />
+      </Helmet>
       <h1>prasanna</h1>
     </div>
   )
