05e9d5e77e731739014cf2f7d5b654bd19c56ede
prasanna <hegdeprasanna118@gmail.com>
2024-04-12 12:44:48 +0530

    css updated

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
-import './login.css'; // You can create a separate CSS file for login page if needed
+import { Link } from 'react-router-dom'; 
+import './login.css'; 
 import { useNavigate } from 'react-router-dom';
 
 
